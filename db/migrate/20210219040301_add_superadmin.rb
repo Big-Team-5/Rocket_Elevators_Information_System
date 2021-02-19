cd46d23c58fe9d6de1f98a810d0ba7740f7e0c92
Dna1444 <d_duquette@hotmail.com>
2021-02-19 00:20:56 -0500

    change default admin

--- db/migrate/20210219040301_add_superadmin.rb
+++ db/migrate/20210219040301_add_superadmin.rb
@@ -1,7 +1,7 @@
 class AddSuperadmin < ActiveRecord::Migration[5.2]
   def change
     User.create! do |u|
-      u.email     = 'Admin@hotmail.com'
+      u.email     = 'admin@hotmail.com'
       u.password  = 'password'
       u.superadmin_role = true
   end
