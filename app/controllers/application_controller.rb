982c3962acda2016057bf55c4a53df727735e777
Dna1444 <d_duquette@hotmail.com>
2021-02-19 00:58:22 -0500

    protect_from_forgery prepend: true, with: :exception skip_before_action :verify_authenticity_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery prepend: true
-
+    protect_from_forgery prepend: true, with: :exception
+    skip_before_action :verify_authenticity_token
     before_action :authenticate_user!
 end
